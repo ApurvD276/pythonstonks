48781e5242a1cbc972b45d0d9884ce1b0e049378
ApurvD276 <100560663+ApurvD276@users.noreply.github.com>
2022-04-30 12:25:22 +0530

    Feature Addition

--- stonks.py
+++ stonks.py
@@ -9,35 +9,36 @@ print('You entered : ' + stock_info['longName'])
 # Stock Info
 market_cap = stock_info['marketCap']
 market_price = stock_info['regularMarketPrice']
-annual_dividend = stock_info['trailingAnnualDividendYield']
+dividend_rate = stock_info['dividendRate']
 
 
 print('Market Capitalization ', 'USD', market_cap)
 print('Market price ', 'USD', market_price)
-print('Annual Dividend Yield', annual_dividend)
+print('Dividend Yield Rate', dividend_rate)
 
 
 # Graph
+validtimeperiods = '1h, 1d, 5d, 1wk, 1mo, 3mo, 5mo, ytd, 1y, 3y, 5y, 10y, max'
 
-def market_end_prices(symbol, period="1mo"):
+
+input_timeperiod = input('Enter the desired time span . The valid time periods are : ' + validtimeperiods)
+
+
+def market_end_prices(symbol, period=input_timeperiod):
     try:
         ticker = yf.Ticker(symbol)
         data = ticker.history(period)
         return data["Close"]
     except Exception as e:
-        print("Error , can't get data", e)
+        print("Error", e)
 
-timeperiod = input('Enter the desired time span  ')
 
 ticker = form
-period = timeperiod = input('Enter the the span  ')
+period = input_timeperiod
 prices_data = market_end_prices(ticker, period)
 
 sns.lineplot(data=prices_data)
 sns.set_theme()
 plt.xticks(rotation=30)
-plt.title(f"Historical stock data of " + stock_info['longName'])
+plt.title(f"Historical stock data of " + stock_info['longName'] + 'of time period ' + input_timeperiod)
 plt.show()
-
-
-
